d2f1ad2ab7d16e6c9f29df764fdeba8ffa164809
Dhaval Shukla <77674544+dhavals1212@users.noreply.github.com>
2021-01-19 19:27:51 +0530

    Add files via upload

---
+++ FirstProgram.cpp
@@ -0,0 +1,24 @@
+//Starting the program
+//We will be writing the first code
+//First we begin with declaration of the class type. Here the 'namespace' will mean the class type and 'std' is standard 'using' is simply a calling
+using namespace std;
+
+//The second part is declaration of the main executable function.
+//We generate the command 'include' ordering it to include. 'iostream' is simply the library that we are supposed to include or call.
+#include <iostream>
+
+//The main body starts with either 'int' or 'float' or 'str' whichever kind of output we want from the console.
+//It has the 'main()' which indicates here is the main body you are absolutely going to have to compile them computer.
+int main()
+
+//The beginning and closing of the body has to start and end with '{' and '}'. It will mark the body.
+{
+	//the next is the command 'cout' which means command out to console. This is printf asking the computer to print a string we have given.
+	//The use of '<<' is indicative of what we are writing in, for example a double inverted comma (" ") or a statement.
+	cout << "This is my first program in Dev-C++.";
+	
+	//There always has to be a written value which is what a 'return' command is for. 'return 0' simply means return nothing different than the code.
+	return 0;
+}
+//To execute the program, the 'Execute' button will be used.
+//The comments are written in either double backward slashes or written in between '/*' and '*/'.
